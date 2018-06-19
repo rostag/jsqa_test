e1f5b71172fc1d481a1a8451dca5d04c3de498c9
Rostyslav Siryk <rosty@Rosty-MacBook-Pro.local>
2018-06-19 09:04:58 +0300

    L12 - Homework added

--- src/app/lessons/12/l-12.js
+++ src/app/lessons/12/l-12.js
@@ -18,12 +18,12 @@
     require('../../util/console');
     let fs = require('fs');
     var path = require('path');
-    var jsonPath = path.join(__dirname, '..', '..', 'data', 'students.json');
+    var jsonPath = path.join(__dirname, '..', '..', 'data', 'config.json');
 
     console.h1('Lesson 12');
     console.h2('Load file:');
 
-    // Read File using absolute path:
+    // Read File using absolute path, asynchronously:
     // fs.readFile('/etc/hosts', 'utf8', function (err, data) {
     //     if (err) {
     //         return console.log(err);
@@ -31,14 +31,31 @@
     //     console.log(data);
     // });
 
-    // 
-
-    // Load Students:
+    // Load Students, asynchronously:
     fs.readFile(jsonPath, 'utf8', function (err, data) {
         if (err) {
             return console.log('Data load error: ', err);
         }
         console.log(data);
     });
+
+    //
+    // Homework
+    //
+    console.h2('Homework Task 12.01');
+    // Points: 2
+    // Please write an example of reading the 'src/app/data/config.json' file by absolute path, synchronously (!)
+
+
+    console.h2('Homework Task 12.02');
+    // Points: 2
+    // Please write an example of reading the 'src/app/data/config.json' file by relative path, asynchronously
+    // Tip: you may need 'path' module for it, see above
+
+
+    console.h2('Homework Task 12.03');
+    // Points: 4
+    // Implement Config module (src/app/config.index.js) as a singleton to be used by other modules
+
 
 }());
