0181a2cd508976791b1417865b519516cfd6ca70
Rostyslav Siryk <rosty@rosty-mbp.synapse.com>
2018-07-05 19:17:48 +0300

    Homework added

--- src/app/lessons/15/l-15.js
+++ src/app/lessons/15/l-15.js
@@ -9,11 +9,6 @@
 
     console.h1('Le**on 15. Regular expressions');
 
-    function testRegexpOnAString(str, regex) {
-        console.log(`\nString: "${str}" RegExp: ${regex}`);
-        console.log('Search result:', str.match(regex));
-    }
-
     let regex = /s{2}/g;
     let regexLocal = /s{2}/;
     let str = 'Lesson 14 is finished, Lesson 15 is not. Assuming you are still working on it.';
@@ -48,8 +43,6 @@
     console.log(regexLocal.exec(str), regexLocal.lastIndex);
     console.log(regexLocal.exec(str), regexLocal.lastIndex);
 
-
-
     // Match:
     console.h3('String.prototype.match() [GLOBAL]');
 
@@ -66,7 +59,6 @@
     console.log(str.match(regexLocal), regexLocal.lastIndex);
 
     // Search
-    
     console.h3('String.prototype.search() [GLOBAL]');
     console.log(str.search(regex), regex.lastIndex);
     console.log(str.search(regex), regex.lastIndex);
@@ -79,7 +71,46 @@
     console.log(str.search(regexLocal), regexLocal.lastIndex);
     console.log(str.search(regexLocal), regexLocal.lastIndex);
 
+    // 
     // Homework
-    // Task 1 - Review Slide # 23
+    // 
+
+    console.h2('Lesson 15 Homework');
+
+    console.h3('Homework Task 15.01. Points: 1');
+    // Task: review Slide # 23 from here...
+    // https://docs.google.com/presentation/d/1qryoG2jeH5XMkkpx-imWl1EtpUnDmRh8dVnf7lfhJNo/edit?usp=sharing
+    // ...and complete this phrase, replacing three dots [...] with needed word:
+    console.log('Parentheses around any part of the regular expression pattern causes that part of the matched substring to be ...');
+
+
+    console.h3('Homework Task 15.02. Points: 1');
+    // Let's practice RegExp.prototype.test() method
+    // Given is the phrase:
+    let phrase = 'Once remembered, the substring can be recalled for other use.';
+    // And regular expression:
+    let reg = /.d/;
+    // Using RegExp.prototype.test method, check whether the given regexp can be found in the given string.
+    // Output the result to the console.
+    // TODO: Do your work here:
+
+
+    console.h3('Homework Task 15.03. Points: 2');
+    // Given is the same phrase as in previous task.
+    // But regular expression now uses global flag:
+    let regg = /.d/g;
+    // Using RegExp.prototype.test method, check how many times the given regexp can be found in the given string.
+    // Output the result to the console.
+    // TODO: Do your work here:
+
+
+    console.h3('Homework Task 15.04. Points: 3');
+    // Let's practice RegExp.prototype.exec() method
+    // Given is the same phrase and regular expressions 'regg', which uses global flag.
+    // Using RegExp.prototype.exec method;
+    
+    // Using the RegExp.prototype.exec() method, list the indexes of all inclusions of this regexp in the string.
+    // Output the result to the console.
+    // TODO: Do your work here:
 
 }());
